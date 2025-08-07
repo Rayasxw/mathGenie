9f037685f55782c7f0cc7e606fd53c6074ad44fe
Rayasxw <maksatbekovarayana@gmail.com>
2025-08-07 20:52:31 +0600

    fixes

--- eslint.config.js
+++ eslint.config.js
@@ -3,12 +3,14 @@ import globals from 'globals';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
 import tseslint from 'typescript-eslint';
-import reactRecommended from 'eslint-plugin-react/configs/recommended.js';
+import react from 'eslint-plugin-react';
 
 export default tseslint.config(
   {
     ignores: ['dist', 'node_modules'],
   },
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
   {
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
@@ -25,12 +27,8 @@ export default tseslint.config(
     plugins: {
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
+      react,
     },
-    extends: [
-      js.configs.recommended,
-      ...tseslint.configs.recommended,
-      reactRecommended,
-    ],
     rules: {
       ...reactHooks.configs.recommended.rules,
       'react-refresh/only-export-components': [
